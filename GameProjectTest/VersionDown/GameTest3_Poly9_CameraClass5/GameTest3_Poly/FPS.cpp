61e28678ad08c093c2b452a96a0c3ea9b830cdd6
Binsuke <msc.radwimps.saiko@hotmail.co.jp>
2019-02-15 17:13:31 +0900

    CameraUpdate

---
+++ GameProjectTest/VersionDown/GameTest3_Poly9_CameraClass5/GameTest3_Poly/FPS.cpp
@@ -0,0 +1,50 @@
+#include "FPS.h"
+
+FPS::FPS() :time(0), frame(0),fFlg(false),deltaFlg(false)
+{
+	//sprintf_s(str, strlen(str), "FPS=");
+	time = timeGetTime();
+}
+
+
+void FPS::Run()
+{
+	frame++;
+	if (timeGetTime() - time > 1000)
+	{
+		time = timeGetTime();
+		Delta = frame;
+		frame = 0;
+		deltaFlg = true;
+	}
+}
+
+
+void FPS::PrintFps(HWND hWnd)
+{
+	if (!fFlg) {
+		sprintf_s(str,"%s","FPS=");
+		SetWindowTextA(hWnd, str);
+		fFlg = true;
+	}
+	
+	if (timeGetTime() - time > 1000)
+	{
+		sprintf_s(str, "fps=%d", frame);
+		SetWindowTextA(hWnd, str);
+	}
+
+}
+
+float FPS::GetDeltaTime()
+{
+	if (deltaFlg) {
+		float tmp = 1.0f / Delta;
+
+		return tmp;
+	}
+	
+	return 0;
+}
+
+
