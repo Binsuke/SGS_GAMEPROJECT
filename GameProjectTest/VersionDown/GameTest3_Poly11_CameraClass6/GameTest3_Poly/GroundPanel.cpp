f4a9c480ec5303676b71bc8841a0c552ab6ed9f3
Binsuke <msc.radwimps.saiko@hotmail.co.jp>
2019-02-16 00:58:48 +0900

    BilBoad

---
+++ GameProjectTest/VersionDown/GameTest3_Poly11_CameraClass6/GameTest3_Poly/GroundPanel.cpp
@@ -0,0 +1,86 @@
+#include "GroundPanel.h"
+
+void GroundPanel::Init(ID3D11Device* indevice, ID3D11DeviceContext* indevicecontext)
+{
+	m_pDevice = indevice;
+	m_pDeviceContext = indevicecontext;
+}
+
+void GroundPanel::Release() {
+	SAFE_RELEASE(m_pVertexBuffer);
+	SAFE_RELEASE(m_pSampleLinear);
+	SAFE_RELEASE(m_pTexture);
+}
+
+HRESULT GroundPanel::CreateVertexBuffer() {
+
+	float size = MyPoly::Poly::PolySize;
+	float halfsize = MyPoly::Poly::PolyHalfSize;
+	SimpleVertex vertices[] =
+	{
+		D3DXVECTOR3(-halfsize,0.0f,-halfsize),D3DXVECTOR2(0,1),//頂点１
+		D3DXVECTOR3(-halfsize,0.0f,halfsize),D3DXVECTOR2(0,0),//頂点２
+		D3DXVECTOR3(halfsize,0.0f,-halfsize),D3DXVECTOR2(1,1),//頂点３
+		D3DXVECTOR3(halfsize,0.0f,halfsize),D3DXVECTOR2(1,0),//頂点４
+	};
+	D3D11_BUFFER_DESC bd; //説明書
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(SimpleVertex) * 4;
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	bd.MiscFlags = 0;
+
+	D3D11_SUBRESOURCE_DATA InitData;
+	InitData.pSysMem = vertices;
+
+	if (FAILED(m_pDevice->CreateBuffer(&bd, &InitData, &m_pVertexBuffer)))
+	{
+		return E_FAIL;
+	}
+	return S_OK;
+}
+
+
+
+
+HRESULT GroundPanel::CreateTexture(std::string filename)
+{
+	//テクスチャー用サンプラー作成
+	D3D11_SAMPLER_DESC SamDesc; //サンプラー用の説明書
+	ZeroMemory(&SamDesc, sizeof(D3D11_SAMPLER_DESC));
+	SamDesc.Filter = D3D11_FILTER_MIN_LINEAR_MAG_POINT_MIP_LINEAR;//どういう設定だろう？
+	SamDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP; //ラップする　繰り返すだと思う
+	SamDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	SamDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP; //U,V,W どの方向もラップする
+
+	m_pDevice->CreateSamplerState(&SamDesc, &m_pSampleLinear);//説明書からサンプラーを作成
+
+	//テクスチャー作成
+	if (FAILED(D3DX11CreateShaderResourceViewFromFile(m_pDevice, filename.c_str(), NULL, NULL, &m_pTexture, NULL)))
+	{
+		MessageBox(NULL, "TextureLoadError", "errot", MB_OK);
+		return E_FAIL;
+	}
+	return S_OK;
+}
+
+void GroundPanel::Render() {
+
+	//バーテックスバッファーセット
+	UINT stride = sizeof(SimpleVertex);
+	UINT offset = 0;
+	
+
+	m_pDeviceContext->IASetVertexBuffers(0, 1, &m_pVertexBuffer, &stride, &offset);
+
+	//テクスチャのサンプラーを登録
+	m_pDeviceContext->PSSetSamplers(0, 1, &m_pSampleLinear);
+	//テクスチャをシェーダーに渡す
+	m_pDeviceContext->PSSetShaderResources(0, 1, &m_pTexture);
+
+	//トポロジーをセット
+	m_pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);//線にするために変更
+
+	//板ポリゴンを描画
+	m_pDeviceContext->Draw(4, 0);
+}
---
+++ GameProjectTest/VersionDown/GameTest3_Poly11_CameraClass6/GameTest3_Poly/MyModel.cpp
@@ -0,0 +1,327 @@
+#include "MyModel.h"
+
+
+MyModel::MyModel(): m_vForward(0,0,1),m_vUp(0,1,0),m_vRight(1,0,0)
+,m_fPolySize(MyPoly::Poly::PolySize), m_fPolyHalfSize(MyPoly::Poly::PolyHalfSize)
+,m_vCenter(0,0,0),m_vPos(0,0,0)
+,m_vLocalForward(0,0,1),m_vLocalRight(1,0,0),m_vLocalUp(0,1,0)
+,m_fAnimTime(0.2f),m_bAnimeFlg(false),m_fAnimeWaitTime(0.0f)
+{
+
+}
+
+void MyModel::InitAdjustment()
+{
+	float tmpx = (float)((m_iModelHalfSizeX ) * m_fPolySize) + m_fPolyHalfSize;
+	float tmpy = (float)((m_iModelHalfSizeY ) * m_fPolySize) + m_fPolyHalfSize;
+	float tmpz = (float)((m_iModelHalfSizeZ ) * m_fPolySize) + m_fPolyHalfSize;
+
+	D3DXMatrixTranslation(&m_mAdjustment, tmpx, tmpy, tmpz);
+}
+
+void MyModel::SetLV1()
+{
+	m_iLV = 1;
+	m_iModelSizeX = 1;
+	m_iModelSizeY = 1;
+	m_iModelSizeZ = 1;
+	m_iModelHalfSizeX = 0;
+	m_iModelHalfSizeY = 0;
+	m_iModelHalfSizeZ = 0;
+	InitAdjustment();
+	D3DXMATRIX tran;
+	D3DXMatrixTranslation(&tran,0, 0, 0);
+	m_lmModelMat.push_back(tran);
+
+	//debug  localZ
+	//D3DXMatrixTranslation(&tran, 0, 0.5, 1);
+	//m_lmModelMat.push_back(tran);
+	UpdateCenter();
+}
+void MyModel::InitLv(int LV)
+{
+	SetLV1();
+	for (int i = 1; i < LV; i++)
+	{
+		LVUp();
+	}
+}
+
+void MyModel::UpdateCenter()
+{
+	D3DXVec3TransformCoord(&m_vCenter, &m_vPos,&m_mAdjustment);
+}
+
+
+
+
+void MyModel::Render(D3DXMATRIX view,D3DXMATRIX proj)
+{
+	m_VertexShader.SetShader(0);
+	m_VertexShader.SetLayout();
+	m_PixelShader.SetShader(0);
+
+	D3DXMATRIX Tran;
+	D3DXMatrixTranslation(&Tran,m_vPos.x, m_vPos.y, m_vPos.z);
+	D3DXMATRIX mW = Tran * m_mAdjustment;
+	for (auto itr=m_lmModelMat.begin(); itr != m_lmModelMat.end(); ++itr)
+	{
+				D3DXMATRIX tmpmatrix;
+				tmpmatrix = (*itr) * mW;
+				m_ConstantBuffer.SetConstantBuffer(tmpmatrix, view, proj);
+				m_ConstantBuffer.SetCBtoPS();
+				m_ConstantBuffer.SetCBtoVS();
+				m_Cube.Render();
+	}
+	UpdateCenter();
+}
+
+void MyModel::Render(D3DXMATRIX view, D3DXMATRIX proj, D3DXMATRIX tran) 
+{
+	m_VertexShader.SetShader(0);
+	m_VertexShader.SetLayout();
+	m_PixelShader.SetShader(0);
+	
+	D3DXMATRIX mW = tran * m_mAdjustment;
+	for (auto itr = m_lmModelMat.begin(); itr != m_lmModelMat.end(); ++itr)
+	{
+		D3DXMATRIX tmpmatrix;
+		tmpmatrix = (*itr) * mW;
+		m_ConstantBuffer.SetConstantBuffer(tmpmatrix, view, proj);
+		m_ConstantBuffer.SetCBtoPS();
+		m_ConstantBuffer.SetCBtoVS();
+		m_Cube.Render();
+	}
+	UpdateCenter();
+}
+void MyModel::Render(D3DXMATRIX view, D3DXMATRIX proj, D3DXMATRIX tran, D3DXMATRIX rotation)
+{
+	m_VertexShader.SetShader(0);
+	m_VertexShader.SetLayout();
+	m_PixelShader.SetShader(0);
+	
+	D3DXMATRIX mW = rotation * tran * m_mAdjustment;
+
+	for (auto itr = m_lmModelMat.begin(); itr != m_lmModelMat.end(); ++itr)
+	{
+		D3DXMATRIX tmpmatrix;
+		tmpmatrix = (*itr) * mW;
+		m_ConstantBuffer.SetConstantBuffer(tmpmatrix, view, proj);
+		m_ConstantBuffer.SetCBtoPS();
+		m_ConstantBuffer.SetCBtoVS();
+		m_Cube.Render();
+	}
+	UpdateCenter();
+}
+
+
+void MyModel::Render(D3DXMATRIX view, D3DXMATRIX proj, D3DXMATRIX tran, D3DXMATRIX rotation,D3DXMATRIX scale)
+{
+	m_VertexShader.SetShader(0);
+	m_VertexShader.SetLayout();
+	m_PixelShader.SetShader(0);
+
+	D3DXMATRIX mW = rotation * tran * m_mAdjustment;
+	for (auto itr = m_lmModelMat.begin(); itr != m_lmModelMat.end(); ++itr)
+	{
+		D3DXMATRIX tmpmatrix;
+		tmpmatrix = scale * (*itr) * mW ;
+		m_ConstantBuffer.SetConstantBuffer(tmpmatrix, view, proj);
+		m_ConstantBuffer.SetCBtoPS();
+		m_ConstantBuffer.SetCBtoVS();
+		m_Cube.Render();
+	}
+	UpdateCenter();
+}
+
+void MyModel:: SetPos(D3DXVECTOR3 vPos) {
+	m_vPos = vPos;
+}
+void MyModel::SetPos(float x, float y, float z) {
+	m_vPos.x = x;
+	m_vPos.y = y;
+	m_vPos.z = z;
+}
+
+void MyModel::MoveWorldForward(float deltaTime)
+{
+	m_vPos += m_fPolySize * m_iModelSizeZ * m_vLocalForward * deltaTime / m_fAnimTime;
+
+	//if (!m_bAnimeFlg) {
+	//	m_bAnimeFlg = true; 
+	//	m_vPos += m_fPolySize * m_iModelSizeZ * m_vLocalForward;
+	//}
+	//if (m_bAnimeFlg) {
+	//	m_fAnimeWaitTime += deltaTime * m_fAnimTime;
+	//	if (m_fAnimTime <= m_fAnimeWaitTime) {
+	//		m_fAnimeWaitTime = 0;
+	//		m_bAnimeFlg = false;
+	//	}
+	//}
+}
+
+void MyModel::MoveWorldBackward(float deltaTime)
+{
+	m_vPos += m_fPolySize * m_iModelSizeZ * -m_vLocalForward * deltaTime / m_fAnimTime;
+
+	//if (!m_bAnimeFlg) {
+	//	m_bAnimeFlg = true;
+	//	m_vPos += m_fPolySize * m_iModelSizeZ * -m_vLocalForward;
+	//}
+	//if (m_bAnimeFlg) {
+	//	m_fAnimeWaitTime += deltaTime * m_fAnimTime;
+	//	if (m_fAnimTime <= m_fAnimeWaitTime) {
+	//		m_fAnimeWaitTime = 0;
+	//		m_bAnimeFlg = false;
+	//	}
+	//}
+}
+
+void MyModel::MoveWorldRight(float deltaTime)
+{
+	m_vPos += m_fPolySize * m_iModelSizeX * m_vLocalRight * deltaTime / m_fAnimTime;
+
+	/*if (!m_bAnimeFlg) {
+		m_bAnimeFlg = true;
+		m_vPos += m_fPolySize * m_iModelSizeX * m_vLocalRight;
+	}
+	if (m_bAnimeFlg) {
+		m_fAnimeWaitTime += deltaTime * m_fAnimTime;
+		if (m_fAnimTime <= m_fAnimeWaitTime) {
+			m_fAnimeWaitTime = 0;
+			m_bAnimeFlg = false;
+		}
+	}*/
+}
+
+void MyModel::MoveWorldLeft(float deltaTime)
+{
+	m_vPos += m_fPolySize * m_iModelSizeX * -m_vLocalRight * deltaTime / m_fAnimTime;
+
+	/*if (!m_bAnimeFlg) {
+		m_bAnimeFlg = true;
+		m_vPos += m_fPolySize * m_iModelSizeX * -m_vLocalRight;
+	}
+	if (m_bAnimeFlg) {
+		m_fAnimeWaitTime += deltaTime * m_fAnimTime;
+		if (m_fAnimTime <= m_fAnimeWaitTime) {
+			m_fAnimeWaitTime = 0;
+			m_bAnimeFlg = false;
+		}
+	}*/
+}
+
+
+void MyModel::Move(float deltaTime, const D3DXVECTOR3& cvForwardVec)
+{
+	if (cvForwardVec == m_vLocalForward) {
+		MoveWorldForward(deltaTime);
+	}
+	if (cvForwardVec == -m_vLocalForward) {
+		MoveWorldBackward(deltaTime);
+	}
+	if (cvForwardVec == m_vLocalRight) {
+		MoveWorldRight(deltaTime);
+	}
+	if (cvForwardVec == -m_vLocalRight) {
+		MoveWorldLeft(deltaTime);
+	}
+}
+void MyModel::LVUp()
+{
+	m_iLV++;
+	int tmpx, tmpy, tmpz;
+
+	tmpx = m_iModelHalfSizeX + 2;
+	tmpy = m_iModelHalfSizeY + 2;
+	tmpz = m_iModelHalfSizeZ + 2;
+
+	for (int x = 0 ; x < (m_iModelSizeX + 2); x++)
+	{
+		for (int y = 0; y < (m_iModelSizeY + 2); y++) {
+			for (int z = 0; z < (m_iModelSizeZ + 2); z++)
+			{
+				if (x == 0 || y == 0 || z == 0 || x == m_iModelSizeX + 1 || y == m_iModelSizeY +1 || z == m_iModelSizeZ + 1) {
+					D3DXMATRIX tran;
+					D3DXMatrixTranslation(&tran, (-m_fPolySize * tmpx + ((x + 1)*m_fPolySize)), (-m_fPolySize * tmpy + ((y + 1)*m_fPolySize)), (-m_fPolySize * tmpz + ((z + 1)*m_fPolySize)));
+
+					m_lmModelMat.push_back(tran);
+				}
+			}
+		}
+	}
+
+	ModelSizeUp();
+	InitAdjustment();
+	UpdateCenter();
+
+
+}
+
+void MyModel::LVDown()
+{
+	if (m_iLV == 1) {//Ž€‚ñ‚¾‚è‚·‚éˆ—‚ð“ü‚ê‚é
+		return;
+	}
+	m_iLV--;
+	for (int x = 0; x < m_iModelSizeX; x++)
+	{
+		for (int y = 0; y < m_iModelSizeY; y++) {
+			for (int z = 0; z < m_iModelSizeZ; z++)
+			{
+				if (x == 0 || y == 0 || z == 0 || x == m_iModelSizeX - 1 || y == m_iModelSizeY - 1 || z == m_iModelSizeZ - 1) {
+					m_lmModelMat.pop_back();
+				}
+			}
+		}
+	}
+	ModelSizeDown();
+	InitAdjustment();
+	UpdateCenter();
+	
+
+}
+
+void MyModel::ModelSizeUp()
+{
+	m_iModelSizeX += 2;
+	m_iModelSizeY += 2;
+	m_iModelSizeZ += 2;
+
+
+	m_iModelHalfSizeX += 1;
+	m_iModelHalfSizeY += 1;
+	m_iModelHalfSizeZ += 1;	
+}
+
+void MyModel::ModelSizeDown() 
+{
+	m_iModelSizeX -= 2;
+	m_iModelSizeY -= 2;
+	m_iModelSizeZ -= 2;
+
+
+	m_iModelHalfSizeX -= 1;
+	m_iModelHalfSizeY -= 1;
+	m_iModelHalfSizeZ -= 1;
+}
+
+float MyModel::GetSizeY()
+{
+	return m_iModelSizeY * m_fPolySize;
+}
+
+D3DXVECTOR3 MyModel:: GetCameraMargin()
+{
+	D3DXVECTOR3 retvec(0,0,0);
+	retvec.y = m_iModelSizeY * m_fPolySize * 0.5f + m_iModelHalfSizeY * m_fPolySize * 0.65f;
+	retvec.z = -(m_iModelSizeZ * m_fPolySize + m_iModelSizeZ * m_fPolySize * 1.3f);
+	return retvec;
+}
+
+D3DXVECTOR3 MyModel::GetCameraTarget()
+{
+	D3DXVECTOR3 retvec = m_vCenter;
+	retvec.y += m_iModelSizeY * m_fPolySize * 0.5;
+	return retvec;
+}
