48ecd4e20e0c1ce37962d8a7e32180b718911f07
Binsuke <msc.radwimps.saiko@hotmail.co.jp>
2019-02-20 06:32:20 +0900

    GlowSystem

---
+++ GameProjectTest/VersionDown/GameTest3_Poly21_colision/GameTest3_Poly/GameScene.h
@@ -0,0 +1,71 @@
+#ifndef GAME_SCENE_H
+#define GAME_SCENE_H
+
+#include "BASE_SCENE.h"
+#include "MyModel.h"
+#include "Ground.h"
+#include "Camera.h"
+#include "MoveUI.h"
+
+#include "FPS.h"
+
+#include "Poly.h"
+
+#include "Player.h"
+
+//extern MAIN* g_pMain;
+
+class GameScene: public BaseScene {
+public:
+	GameScene();
+	~GameScene();
+	BaseScene::eScene App();
+private:
+	void Init();
+	void Release();
+	//void InitD3D();
+	void Render();
+	HRESULT InitPolygon();
+	HRESULT InitShader();
+
+	enum eMoveVec {
+		None = 0,
+		Forward = 1,
+		Backward = 2,
+		Right = 3,
+		Left = 4,
+	};
+
+	void Input();
+
+	void MoveUpdate(eMoveVec Vec);
+
+	bool MoveUpdateA(eMoveVec Vec);
+
+	bool Colision(MyModel* pModelA, MyModel* pModelB);
+
+	bool ColisionA(MyModel* pModelA, MyModel* pModelB);
+
+	//void CheckSameLVPolygonColision();
+
+	Camera* m_pCamera;
+		void InitModel();//この辺はステージなんかを作った場合はシーンごとに分けてシーンにモデルを持たせて、初期化するようにするのがいいかな
+	void ReleaseModel();
+
+
+	eMoveVec m_eMoveVec;
+	
+	MyPlayer* m_pTestModel;
+	enum {
+		EnemyNum = 120,
+	};
+	MyModel* m_pEnemyModel[EnemyNum];
+	MyGround* m_pGround;
+
+	MoveUI* m_pMoveUI;
+	bool UI_RenderFlg;
+
+	float view;
+};
+
+#endif
---
+++ GameProjectTest/VersionDown/GameTest3_Poly21_colision/GameTest3_Poly/Player.h
@@ -0,0 +1,43 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "MyModel.h"
+
+class MyPlayer : public MyModel {
+public:
+	MyPlayer();
+	//~MyPlayer();
+	void GlowStackCube(int CubeNum);
+	bool Damage(int iLV);
+
+	void Render(D3DXMATRIX view, D3DXMATRIX proj);
+
+	bool GetDamegaFlg();
+
+	void InitDamagePolygon(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+
+	void SameCubeColisionCheck(MyModel* pModel);
+	void SameCubeColisionCheckEnd();
+private:
+	bool GlowInitFlg;
+	int m_iNextLevel;
+	int m_iCubeStack;
+	
+	bool m_bDamageInitFlg;
+	bool m_bDamageFlg;
+
+	bool m_bDamageEndFlg;
+	float m_fDamageAnimationTime;
+
+	bool m_bGlowFlg;
+
+	DWORD m_dwDamageAnimationWaitTime;
+
+	float m_fPrevAnimTime;
+
+	MyPoly::Poly m_DamageCube;
+
+	void DamageRender(D3DXMATRIX view, D3DXMATRIX proj);
+
+};
+#endif
