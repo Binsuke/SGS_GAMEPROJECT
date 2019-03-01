fd6dc02248090fc84a4ab21dcc9969dd940141c8
Binsuke <msc.radwimps.saiko@hotmail.co.jp>
2019-03-01 11:02:23 +0900

    Etc

---
+++ GameProjectTest/VersionDown/GameTest3_Poly28_etc  cameralimienable/GameTest3_Poly/BASE_SCENE.h
@@ -0,0 +1,33 @@
+#ifndef BASE_SCENE
+#define BASE_SCENE
+
+//ヘッダーファイルのインクルード
+#include <Windows.h>	
+//ウィンドウプログラミングに必要なもの
+#include <d3d11.h>		
+//d3d11
+#include <d3dx10.h>
+//d3dx系列
+#include <d3dx11.h>		
+//d3dx系列
+#include <d3dcompiler.h>
+
+
+class BaseScene {
+public:
+	enum eScene {//正直いい方法が思いつかなかった
+		Title,
+		Game,
+		Result,
+		End,
+	};
+	virtual BaseScene::eScene App() = 0;
+	virtual void Release() = 0;
+
+protected:
+	virtual void Init() = 0;
+	//virtual void InitD3D() = 0;
+	bool m_bInitFlg;
+};
+
+#endif
